217f60667973dfe3d8ff991e73a3e3f78360ad6a
Kate Korsaro <kate.korsaro@gmail.com>
2023-12-19 14:08:26 +0100

    UPD code refactor into modules

---
+++ src/roller/roller/parse.rs
@@ -0,0 +1,61 @@
+#[test]
+fn standard() {
+    let r: super::Roller = String::from("3d6").parse().unwrap();
+    assert_eq!(r.dice, 3);
+    assert_eq!(r.sides, 6);
+}
+
+#[test]
+fn single_die() {
+    let r: super::Roller = String::from("d10").parse().unwrap();
+    assert_eq!(r.dice, 1);
+    assert_eq!(r.sides, 10);
+}
+
+#[test]
+fn standard_and_modifier() {
+    let r: super::Roller = String::from("3d6+4").parse().unwrap();
+    assert_eq!(r.dice, 3);
+    assert_eq!(r.sides, 6);
+    assert_eq!(r.modifier, Some(4));
+}
+
+#[test]
+fn standard_and_negative_modifier() {
+    let r: super::Roller = String::from("3d6-4").parse().unwrap();
+    assert_eq!(r.dice, 3);
+    assert_eq!(r.sides, 6);
+    assert_eq!(r.modifier, Some(-4));
+}
+
+#[test]
+fn single_die_with_modifier() {
+    let r: super::Roller = String::from("d6-4").parse().unwrap();
+    assert_eq!(r.dice, 1);
+    assert_eq!(r.sides, 6);
+    assert_eq!(r.modifier, Some(-4));
+}
+
+#[test]
+fn success_threshold() {
+    let r: super::Roller = String::from("3d6 sc5").parse().unwrap();
+    assert_eq!(r.dice, 3);
+    assert_eq!(r.sides, 6);
+    assert_eq!(r.success_threshold, Some(5));
+}
+
+#[test]
+fn explode_threshold() {
+    let r: super::Roller = String::from("3d6 ex4").parse().unwrap();
+    assert_eq!(r.dice, 3);
+    assert_eq!(r.sides, 6);
+    assert_eq!(r.explode_threshold, Some(4));
+}
+
+#[test]
+fn max_x_of_y () {
+    let r: super::Roller = String::from("4d6 max3").parse().unwrap();
+    assert_eq!(r.dice, 4);
+    assert_eq!(r.sides, 6);
+    assert_eq!(r.take_max, Some(3));
+}
---
+++ src/roller/roller/roll.rs
@@ -0,0 +1,95 @@
+#[test]
+fn standard() {
+    let mut r: super::Roller = String::from("3d6").parse().unwrap();
+    let roll_result = r.roll();
+    assert_eq!(roll_result.dice.len(), 3);
+    for die in roll_result.dice {
+        assert!(die > 0 && die < 7);
+    }
+    assert!(roll_result.outcome > 0 && roll_result.outcome < 19);
+}
+
+#[test]
+fn single_die() {
+    let mut r: super::Roller = String::from("d20").parse().unwrap();
+    for _ in 1..=1000 {
+        let roll_result = r.roll();
+        assert_eq!(roll_result.dice.len(), 1);
+        assert!(roll_result.outcome > 0 && roll_result.outcome < 21);
+    }
+}
+
+#[test]
+fn standard_with_modifier() {
+    let mut r: super::Roller = String::from("1d20+20").parse().unwrap();
+    for _ in 1..=1000 {
+        let roll_result = r.roll();
+        assert_eq!(roll_result.dice.len(), 1);
+        assert!(roll_result.outcome > 20 && roll_result.outcome < 41);
+    }
+}
+
+#[test]
+fn standard_with_negative_modifier() {
+    let mut r: super::Roller = String::from("1d20-20").parse().unwrap();
+    for _ in 1..=1000 {
+        let roll_result = r.roll();
+        assert_eq!(roll_result.dice.len(), 1);
+        assert!(roll_result.outcome > -20 && roll_result.outcome < 1);
+    }
+}
+
+#[test]
+fn standard_single_die_with_negative_modifier() {
+    let mut r: super::Roller = String::from("d20-20").parse().unwrap();
+    for _ in 1..=1000 {
+        let roll_result = r.roll();
+        assert_eq!(roll_result.dice.len(), 1);
+        assert!(roll_result.outcome > -20 && roll_result.outcome < 1);
+    }
+}
+#[test]
+fn standard_success_counting() {
+    let mut r: super::Roller = String::from("6d6 sc1").parse().unwrap();
+    for _ in 1..=1000 {
+        let roll_result = r.roll();
+        assert_eq!(roll_result.outcome, 6);
+    }
+}
+#[test]
+fn standard_exploding() {
+    let mut r: super::Roller = String::from("1d6 ex6").parse().unwrap();
+    for _ in 1..=1000 {
+        let roll_result = r.roll();
+        roll_result.dice.iter()
+            .take(roll_result.dice.len()-1)
+            .for_each(|x| assert!(*x == 6));
+        assert!(*roll_result.dice.last().unwrap() < 6);
+    }
+}
+#[test]
+fn exploding_success_threshold () {
+    let mut r: super::Roller = String::from("1d6 ex6 sc6").parse().unwrap();
+    for _ in 1..=1000 {
+        let roll_result = r.roll();
+        roll_result.dice.iter()
+            .take(roll_result.dice.len()-1)
+            .for_each(|x| assert!(*x == 1));
+        assert!(*roll_result.dice.last().unwrap() == 0);
+    }
+}
+
+#[test]
+fn max_x_of_y () {
+    let mut r: super::Roller = String::from("4d6 max3").parse().unwrap();
+    for _ in 1..=1000 {
+        let roll_result = r.roll();
+        assert_eq!(roll_result.dice.len(), 4);
+        let mut results = roll_result.dice.clone();
+        results.sort();
+        let max3: i32 = results.iter()
+            .take(3)
+            .sum();
+        assert_eq!(max3, roll_result.outcome);
+    }
+}
